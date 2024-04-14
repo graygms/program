c1d4df33e0d7f3b4961d969cf6b1aa779df04e9f
Graham Brooks <grahambrooks.coarses@gmail.com>
2024-04-14 18:32:30 +0100

    first commit

---
+++ my project/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+namespace project
+{
+    class Rectangle
+    {
+     
+            double length;
+            double width;
+
+            public void acceptdetails(){
+                length=4.5;
+                width=3.5;
+            }
+        
+    public double GetArea(){
+        return length*width;
+    }
+    public void Display(){
+       
+        Console.WriteLine("Length:{0}",length);
+         Console.WriteLine("Width:{0}",width);
+          Console.WriteLine("Area:{0}",GetArea());
+    }
+        }
+        class ExecuteRectangle{
+            static void Main(string[] args){
+                Rectangle r=new Rectangle();
+                r.acceptdetails();
+                r.Display();
+                Console.ReadLine();
+
+            }
+        }
+    }
+
